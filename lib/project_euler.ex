ced7da6a33c71757f7ea43b61abcfec77dd58477
Cameron Bass <cam@collectiveidea.com>
2017-06-24 13:29:38 -0400

    Add problem 4 for project Euler

--- lib/project_euler.ex
+++ lib/project_euler.ex
@@ -1,6 +1,6 @@
 defmodule ProjectEuler do
 
-  # Problem 1
+  # Problem 1 - Multiples Of 3 and 5
 
   def solve(limit) do
     range = 1..limit
@@ -14,7 +14,22 @@ defmodule ProjectEuler do
 
   # ---------------------------------
 
-  # Problem 2
+  # Problem 2 - Even Fibonacci Numbers
+  # def fib(n) when n <= 1, do: n
+  # def fib(n) do
+  #   Enum.each(fib(n - 1) + fib(n - 2))
+  # end
 
-  
+
+  # problem 4 - Largest Palindrome Product
+    def is_palindrome?(n) do
+      Integer.to_string(n) == Integer.to_string(n)|> String.reverse
+    end
+
+    def multiply do
+      range_1 = 100..999
+      range_2 = 100..999
+
+      Enum.max(for n <- range_1, m <- range_2, is_palindrome?(n * m), do: n * m)
+    end
 end
--- test/project_euler_test.exs
+++ test/project_euler_test.exs
@@ -4,4 +4,14 @@ defmodule ProjectEulerTest do
   test "for problem 1" do
     assert ProjectEuler.solve(9) == 23
   end
+
+  # test "for problem 2" do
+  #   assert ProjectEuler.fib(1) == 1
+  #   assert ProjectEuler.fib(0) == 0
+  #   assert ProjectEuler.fib(7) == 10
+  # end
+
+  test "for problem 4" do
+    assert ProjectEuler.multiply == 906609
+  end
 end
