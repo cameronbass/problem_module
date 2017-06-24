96eb41abe620005a86fa956151c0885f90ba12b9
Cameron Bass <cam@collectiveidea.com>
2017-06-24 14:56:45 -0400

    add styles to the module

--- lib/project_euler.ex
+++ lib/project_euler.ex
@@ -1,7 +1,6 @@
 defmodule ProjectEuler do
 
   # Problem 1 - Multiples Of 3 and 5
-
   def solve(limit) do
     range = 1..limit
     multiple_of_3_or_5? = fn(n) -> (rem(n, 3) == 0 || rem(n, 5) == 0) end
@@ -12,24 +11,15 @@ defmodule ProjectEuler do
     IO.puts solve(999)
   end
 
-  # ---------------------------------
+  # Problem 4 - Largest Palindrome Product
+  def is_palindrome?(n) do
+    Integer.to_string(n) == Integer.to_string(n)|> String.reverse
+  end
 
-  # Problem 2 - Even Fibonacci Numbers
-  # def fib(n) when n <= 1, do: n
-  # def fib(n) do
-  #   Enum.each(fib(n - 1) + fib(n - 2))
-  # end
+  def multiply do
+    Enum.max(for n <- 100..999, m <- 100..999, is_palindrome?(n * m), do: n * m)
+  end
 
+  # Problem 5 - Smallest Multiple
 
-  # problem 4 - Largest Palindrome Product
-    def is_palindrome?(n) do
-      Integer.to_string(n) == Integer.to_string(n)|> String.reverse
-    end
-
-    def multiply do
-      range_1 = 100..999
-      range_2 = 100..999
-
-      Enum.max(for n <- range_1, m <- range_2, is_palindrome?(n * m), do: n * m)
-    end
 end
